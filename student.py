6846dca37640da9409fbedcd4b4f6c0d82c26249
keertigururaj2006 <keertigururaj2006@gmail.com>
2025-11-22 10:03:50 +0530

    student.py

--- student.py
+++ student.py
@@ -1,9 +1,10 @@
 import sys
-
+        # Check if correct number of arguement
 if len(sys.argv) != 3:
     print("Usage: python student.py <name> <rollno>")
     sys.exit(1)
 
+#sys.argv[0] is always the program name
 script_name = sys.argv[0]
 name = sys.argv[1]
 rollno = sys.argv[2]

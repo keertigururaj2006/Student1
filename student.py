76061eba8ed2bae2d170fd37887e77047646d648
keertigururaj2006 <keertigururaj2006@gmail.com>
2025-11-22 10:37:16 +0530

    Update student.py

--- student.py
+++ student.py
@@ -1,10 +1,7 @@
 import sys
         
 if len(sys.argv) != 3:
-script_name = sys.argv[0]
-name = sys.argv[1]
-rollno = sys.argv[2]
-
-print("Script Name:", script_name)
+        name = sys.argv[1]
+        rollno = sys.argv[2]
 print("Student Name:", name)
 print("Roll Number:", rollno)
